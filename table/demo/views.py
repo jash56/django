4b39049f4e088d9fdb9ad2bb94bd21f40df17176
jash56 <jash.gaglani99@gmail.com>
2020-05-08 18:30:12 +0530

    first verion

---
+++ table/demo/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'hello.html', {'name':'JASH'})
+
+def tab(request):
+    val = int(request.GET['even'])
+    '''ans =  list()
+    ans1 = list()
+    ans2 = list()
+    c = 1
+
+    for i in range(1, (val-1)):
+        if (i % 2 == 0):
+            ans.append(i)
+
+    for i in ans:
+        ans1.append(i)
+        if (c % 13 == 0):
+            ans2.append(ans1)
+            ans1 = []  
+        c = c + 1
+    ans2.append(ans1)
+
+    return render(request, 'result.html', {'val': ans2})'''
+    return render(request, 'result.html', {'val': val})
